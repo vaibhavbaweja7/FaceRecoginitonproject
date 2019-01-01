8de6bbab7e26de9d801de5ab3b52fa688776f1e6
Vaibhav Baweja <vaibhavbwj9@gmail.com>
2019-01-01 18:38:09 +0530

    Add files via upload

---
+++ imageshow.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 18:05:58 2019
+
+@author: lenovo
+"""
+
+import cv2
+img = cv2.imread("D:\E.jpg")
+cv2.imshow('Eminem',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
